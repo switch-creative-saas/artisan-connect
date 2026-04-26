abb3f0b5ccbc26646c1a79f0359fae72e215bff2
Fredrick Tariq <tariqfredrick@gmail.com>
2026-04-26 16:07:39 +0100

    db infinity

--- backend/config.php
+++ backend/config.php
@@ -1,10 +1,10 @@
 <?php
 // Beginner-friendly config: update these values to match your local MySQL setup.
 return [
-  'db_host' => getenv('DB_HOST'),
-  'db_name' => getenv('DB_NAME'),
-  'db_user' => getenv('DB_USER'),
-  'db_pass' => getenv('DB_PASS'),
+  'db_host' => 'sql100.infinityfree.com',
+  'db_name' => 'if0_41738056_artisanconnect',
+  'db_user' => 'if0_41738056',
+  'db_pass' => 'BFdpbo8gH8v',
   'db_charset' => 'utf8mb4',
 ];
 
